2906219c4aeb011d574ad0a5e4e6ec26a8fa18e8
jungjaeok <yangpajuice@gmail.com>
2020-04-10 14:26:13 +0900

    com

---
+++ ReservationCrawling/src/main/java/com/tistory/yangpajuice/rc/ReservationCrawlingApplication.java
@@ -0,0 +1,13 @@
+package com.tistory.yangpajuice.rc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationCrawlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationCrawlingApplication.class, args);
+	}
+
+}
---
+++ ReservationCrawling/src/test/java/com/tistory/yangpajuice/rc/ReservationCrawlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistory.yangpajuice.rc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationCrawlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
