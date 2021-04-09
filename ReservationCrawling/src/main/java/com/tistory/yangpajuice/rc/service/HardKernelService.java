89de6595c2d963d5ec01bcfab32fb7d34eae1e3a
yangpajuice <yangpajuice@DESKTOP-BKECNF3>
2021-04-09 20:23:29 +0900

    구조변경

--- ReservationCrawling/src/main/java/com/tistory/yangpajuice/rc/service/HardKernelService.java
+++ ReservationCrawling/src/main/java/com/tistory/yangpajuice/rc/service/HardKernelService.java
@@ -43,45 +43,67 @@ public class HardKernelService implements IService {
 
 		try {
 			Map<String, WebPageItem> webPageItemListFromDb = getWebPageItemFromDb();
-			Map<String, WebPageItem> webPageItemListFromWeb = getWebPageItemFromWeb();
+			
+			ConfigParam param = new ConfigParam();
+			param.setSectId(CodeConstants.SECT_ID_HARDKERNEL);
+			param.setKeyId(CodeConstants.KEY_ID_URL);
+			List<ConfigItem> configItemList = dbService.getConfigItemList(param);
+			if (configItemList != null && configItemList.size() > 0) {
+				for (ConfigItem configItem : configItemList) {
+					String url = configItem.getValue();
+					logger.info("URL = " + url);
 
-			Iterator<String> keys = webPageItemListFromWeb.keySet().iterator();
-			while (keys.hasNext()) {
-				String key = keys.next();
-				WebPageItem webPageItemFromWeb = webPageItemListFromWeb.get(key);
-				WebPageItem webPageItemFromDb = webPageItemListFromDb.get(key);
+					Document doc = Jsoup.connect(url).userAgent(
+							"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36")
+							.referrer("http://www.google.com").get();
 
-				if (webPageItemFromDb == null) { // new item
-					dbService.insertWebPageItem(webPageItemFromWeb);
-					
-					String msg = "New [" + webPageItemFromWeb.getMainCategory() + "] \n";
-					msg += webPageItemFromWeb.getSubCategory() + "\n";
-					msg += webPageItemFromWeb.getUrl();
-					telegram.sendMessage(telegramHardkernelAlarmConfig, msg);
+					Map<String, WebPageItem> webPageItemListFromWeb = getWebPageItemFromWeb(doc);
+					if (webPageItemListFromWeb == null || webPageItemListFromWeb.size() == 0) {
+						continue;
+					}
 
-				} else { // item exist
-					if (webPageItemFromWeb.getSubCategory().equals(webPageItemFromDb.getSubCategory()) == true
-							&& webPageItemFromWeb.getSubject().equals(webPageItemFromDb.getSubject()) == true
-							&& webPageItemFromWeb.getArticle().equals(webPageItemFromDb.getArticle()) == true) {
-						// same Item
+					Iterator<String> keys = webPageItemListFromWeb.keySet().iterator();
+					while (keys.hasNext()) {
+						String key = keys.next();
+						WebPageItem webPageItemFromWeb = webPageItemListFromWeb.get(key);
+						WebPageItem webPageItemFromDb = webPageItemListFromDb.get(key);
 
-					} else { // Item is updated
-						// 데이터가 변경되는 경우 기존 데이터를 덮어씌우지 않고 새로 입력한다.
-						// 기존의 데이터 ID를 1씩 증가시킨다. 최신데이터 ID는 항상 0이 되도록 한다.
-						dbService.updateWebPageItemIdIncrease(webPageItemFromWeb);
-						dbService.insertWebPageItem(webPageItemFromWeb);
-						
-						// 가격이 변경되는 경우
-						if (webPageItemFromWeb.getSubCategory().equals(webPageItemFromDb.getSubCategory()) == false) {
-							String msg = "[" + webPageItemFromWeb.getMainCategory() + "] \n";
-							msg += webPageItemFromDb.getSubCategory() + " -> " + webPageItemFromWeb.getSubCategory() + "\n";
+						if (webPageItemFromDb == null) { // new item
+							dbService.insertWebPageItem(webPageItemFromWeb);
+							
+							String msg = "New [" + webPageItemFromWeb.getMainCategory() + "] \n";
+							msg += webPageItemFromWeb.getSubCategory() + "\n";
 							msg += webPageItemFromWeb.getUrl();
 							telegram.sendMessage(telegramHardkernelAlarmConfig, msg);
+
+						} else { // item exist
+							if (webPageItemFromWeb.getSubCategory().equals(webPageItemFromDb.getSubCategory()) == true
+									&& webPageItemFromWeb.getSubject().equals(webPageItemFromDb.getSubject()) == true
+									&& webPageItemFromWeb.getArticle().equals(webPageItemFromDb.getArticle()) == true) {
+								// same Item
+
+							} else { // Item is updated
+								// 데이터가 변경되는 경우 기존 데이터를 덮어씌우지 않고 새로 입력한다.
+								// 기존의 데이터 ID를 1씩 증가시킨다. 최신데이터 ID는 항상 0이 되도록 한다.
+								dbService.updateWebPageItemIdIncrease(webPageItemFromWeb);
+								dbService.insertWebPageItem(webPageItemFromWeb);
+								
+								// 가격이 변경되는 경우
+								if (webPageItemFromWeb.getSubCategory().equals(webPageItemFromDb.getSubCategory()) == false) {
+									String msg = "[" + webPageItemFromWeb.getMainCategory() + "] \n";
+									msg += webPageItemFromDb.getSubCategory() + " -> " + webPageItemFromWeb.getSubCategory() + "\n";
+									msg += webPageItemFromWeb.getUrl();
+									telegram.sendMessage(telegramHardkernelAlarmConfig, msg);
+								}
+							}
 						}
 					}
+					
+					Thread.sleep(2000);
 				}
+			} else {
+				logger.error("URL Config does not exist.");
 			}
-
 		} catch (Exception e) {
 			logger.error("An exception occurred!", e);
 		}
@@ -111,41 +133,6 @@ public class HardKernelService implements IService {
 
 	private String getKey(WebPageItem item) {
 		return item.getMainCategory();
-	}
-
-	private Map<String, WebPageItem> getWebPageItemFromWeb() {
-		Map<String, WebPageItem> rtnValue = new TreeMap<String, WebPageItem>(Collections.reverseOrder());
-
-		try {
-			ConfigParam param = new ConfigParam();
-			param.setSectId(CodeConstants.SECT_ID_HARDKERNEL);
-			param.setKeyId(CodeConstants.KEY_ID_URL);
-			List<ConfigItem> configItemList = dbService.getConfigItemList(param);
-			if (configItemList != null && configItemList.size() > 0) {
-				for (ConfigItem configItem : configItemList) {
-					String url = configItem.getValue();
-					logger.info("URL = " + url);
-
-					Document doc = Jsoup.connect(url).userAgent(
-							"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36")
-							.referrer("http://www.google.com").get();
-
-					Map<String, WebPageItem> tmpRtnValue = getWebPageItemFromWeb(doc);
-					if (tmpRtnValue != null && tmpRtnValue.size() > 0) {
-						rtnValue.putAll(tmpRtnValue);
-					}
-
-					Thread.sleep(2000);
-				}
-			} else {
-				logger.error("URL Config does not exist.");
-			}
-
-		} catch (Exception e) {
-			logger.error("An exception occurred!", e);
-		}
-
-		return rtnValue;
 	}
 
 	private Map<String, WebPageItem> getWebPageItemFromWeb(Document doc) {
